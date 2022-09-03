c86f107c4d538af780e5fb9cb5554ddc713662fd
mega16 <mega16@aliyun.com>
2022-09-03 21:16:37 +0800

    feat(custom): cz

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,28 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'plugin:react/recommended',
-        "plugin:react-hooks/recommended",
-        'plugin:@typescript-eslint/recommended',
-        "prettier"
-    ],
-    parserOptions: {
-        ecmaVersion:  2018,  // 帮助转化最先进的ECMAScript功能
-        sourceType:  'module',  // 允许imports的用法
-        ecmaFeatures:  {
-            jsx:  true,  // JSX兼容
-        },
-    },
-    rules:  {
-        "@typescript-eslint/no-var-requires": "off",
-
-    },
-    settings:  {
-        react:  {
-          version:  'detect',  // 告诉eslint-plugin-react自动检测最新版本的react
-        },
-    },
-}
+	env: {
+		node: true,
+		browser: true,
+	},
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+	],
+	parserOptions: {
+		ecmaVersion: 2018, // 帮助转化最先进的ECMAScript功能
+		sourceType: 'module', // 允许imports的用法
+		ecmaFeatures: {
+			jsx: true, // JSX兼容
+		},
+	},
+	rules: {
+		'@typescript-eslint/no-var-requires': 'off',
+	},
+	settings: {
+		react: {
+			version: 'detect', // 告诉eslint-plugin-react自动检测最新版本的react
+		},
+	},
+};
