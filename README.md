30fb6f54f6d3e3a5e8ddd30d1d5ed3f3b670ba35
mega16 <mega16@aliyun.com>
2022-09-03 21:39:05 +0800

    docs(custom): readme.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # webpack5-react18-typescript
-基于Webpack5 + React18 + typescript的标准化前端脚手架
+
+基于 Webpack5 + React18 + typescript 的标准化前端应用
+
+- 技术栈: webpack5 + React18 + TS
+- 工程化: eslint + prettier + husky + git hooks
+- 支持图片、less、sass、fonts、数据资源(JSON、csv、tsv 等)、Antd 按需加载以及主题
+- 支持热更新、资源压缩、代码分离（动态导入、懒加载等）、缓存、devServer
--- webpack.config.js
+++ webpack.config.js
@@ -7,133 +7,130 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
-
-
 const { DEV, DEBUG } = process.env;
 
 process.env.BABEL_ENV = DEV ? 'development' : 'production';
 process.env.NODE_ENV = DEV ? 'development' : 'production';
 
-
 module.exports = {
-    entry: './src/index.tsx',
+	entry: './src/index.tsx',
 
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: '[name].bundle.js',
-        clean: true
-    },
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: '[name].bundle.js',
+		clean: true,
+	},
 
-    mode: DEV ? 'development' : 'production',
+	mode: DEV ? 'development' : 'production',
 
-    devtool: DEV && 'source-map',
+	devtool: DEV && 'source-map',
 
-    devServer: {
-        port: 8080
-    },
+	devServer: {
+		port: 8080,
+	},
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader',
-                options: {
-                    // 项目编译时就不会进行类型检查，也不会输出声明文件.默认为 false
-                    transpileOnly: true
-                }
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(less|css)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 2,
-                            sourceMap: !!DEV,
-                        }
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            sourceMap: !!DEV,
-                            lessOptions: {
-                                // 定制antd 主题
-                                modifyVars: {
-                                    'primary-color': '#1da57a',
-                                },
-                                // 支持js
-                                javascriptEnabled: true,
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.png/,
-                type: "asset/resource"
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ]
-    },
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+				options: {
+					// 项目编译时就不会进行类型检查，也不会输出声明文件.默认为 false
+					transpileOnly: true,
+				},
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(less|css)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 2,
+							sourceMap: !!DEV,
+						},
+					},
+					{
+						loader: 'less-loader',
+						options: {
+							sourceMap: !!DEV,
+							lessOptions: {
+								// 定制antd 主题
+								modifyVars: {
+									'primary-color': '#1da57a',
+								},
+								// 支持js
+								javascriptEnabled: true,
+							},
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
 
-    resolve: {
-        modules: ['node_modules'],
-        extensions: ['.json', '.js', '.jsx', '.ts', '.tsx', '.less'],
-    },
+	resolve: {
+		modules: ['node_modules'],
+		extensions: ['.json', '.js', '.jsx', '.ts', '.tsx', '.less'],
+	},
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, '/src/index.html'),
-            inject: 'body',
-        }),
-        // new CleanWebpackPlugin(),
-        DEBUG && new BundleAnalyzerPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[name].css',
-        }),
-        new ESLintPlugin(),
-        new ForkTsCheckerWebpackPlugin()
-    ].filter(Boolean),
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, '/src/index.html'),
+			inject: 'body',
+		}),
+		// new CleanWebpackPlugin(),
+		DEBUG && new BundleAnalyzerPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[name].css',
+		}),
+		new ESLintPlugin(),
+		new ForkTsCheckerWebpackPlugin(),
+	].filter(Boolean),
 
-    optimization: {
-        usedExports: true,
+	optimization: {
+		usedExports: true,
 
-        minimizer: [
-            new TerserPlugin({
-                parallel: false,
-                terserOptions: {
-                    output: {
-                        comments: false,
-                    }
-                }
-            }),
+		minimizer: [
+			new TerserPlugin({
+				parallel: false,
+				terserOptions: {
+					output: {
+						comments: false,
+					},
+				},
+			}),
 
-            new OptimizeCSSAssetsPlugin({})
-        ],
-        minimize: !DEV,
-        splitChunks: {
-            minSize: 500000,
-            cacheGroups: {
-                vendors: false
-            }
-        }
-    }
-}
+			new OptimizeCSSAssetsPlugin({}),
+		],
+		minimize: !DEV,
+		splitChunks: {
+			minSize: 500000,
+			cacheGroups: {
+				vendors: false,
+			},
+		},
+	},
+};
